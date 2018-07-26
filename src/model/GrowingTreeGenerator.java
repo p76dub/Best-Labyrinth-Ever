868393fd10bbc3f06bbbf9c58a07afff1bae289e
Pierre DUBAILLAY <pierredubaillay@orange.fr>
2018-07-26 13:39:25 +0200

    Updated doc on GrowingTreeGenerator

--- src/model/GrowingTreeGenerator.java
+++ src/model/GrowingTreeGenerator.java
@@ -7,13 +7,54 @@ import util.Direction;
 
 import java.util.*;
 
+/**
+ * Générateur de labyrinthe utilisant la méthode du GrowingTree :
+ * <ol>
+ *     <li>On sélectionne une cellule au hasard dans le labyrinthe : c'est le point de départ</li>
+ *     <li>On ajoute cette cellule à la liste des cellules dites ouvertes, c'est à dire celles dont au moins une cellule
+ *     voisine n'a pas encore été visitée</li>
+ *     <li>Tant que la liste des cellules ouvertes n'est pas vide :
+ *          <ol>
+ *              <li>On sélectionne une cellule</li>
+ *              <li>Si la cellule choisie n'a plus de cellules voisines disponibles, elle est supprimée de la liste des
+ *              cellules ouvertes et placée dans celle des cellules fermées.</li>
+ *              <li>Sinon, on choisit une cellule adjacente non visitée et on l'ajoute à la liste des cellules ouvertes
+ *              </li>
+ *          </ol>
+ *     </li>
+ * </ol>
+ * La sélection de la prochaine cellule a explorer peut se faire de la manière suivante :
+ * <ul>
+ *     <li>En choisissant toujours la dernière cellule explorée</li>
+ *     <li>En choisissant une cellule au hasard</li>
+ *     <li>En choisissant la dernière cellule explorée et, de temps en temps, une cellule au hasard</li>
+ *     <li>En choisissant une cellule parmis les N dernières explorées</li>
+ * </ul>
+ */
 public class GrowingTreeGenerator implements IMazeGenerator {
     // INTERNAL CLASS
+    /**
+     * Représente une entrée utilisée pour maintenir la liste des pièces visitées / fermées. Les objets de type Entry
+     * sont non mutables.
+     * @inv getRoom() != null
+     */
     private class Entry {
         private final int x;
         private final int y;
         private final IRoom room;
 
+        /**
+         * Créer une nouvelle entrée avec la pièce fournie, située dans la labyrinthe à la position (x,y)
+         * @param x position (colonne) de la pièce
+         * @param y position (ligne) de la pièce
+         * @param room la pièce stockée dans l'entrée
+         * @pre room != null
+         * @post <pre>
+         *     getRoom().equals(room)
+         *     getX() == x
+         *     getY() == y
+         * </pre>
+         */
         public Entry(int x, int y, IRoom room) {
             if (room == null) {
                 throw new NullPointerException();
@@ -35,6 +76,12 @@ public class GrowingTreeGenerator implements IMazeGenerator {
             return room;
         }
 
+        /**
+         * Comparaison entre deux objets, la cible et un autre. Indique si les deux objets sont identiques, c'est à dire
+         * que la pièce est les coordonnées sont identiques.
+         * @param obj l'objet avec lequels la comparaison est faite
+         * @return true si les deux objets sont identiques, false sinon
+         */
         @Override
         public boolean equals(Object obj) {
             if (obj != null && obj.getClass().equals(this.getClass())) {
@@ -75,17 +122,17 @@ public class GrowingTreeGenerator implements IMazeGenerator {
 
     @Override
     public void generate() {
-        // Keep track of visited rooms
-        Set<Entry> opened = new HashSet<>();
+        // Garder une trace des pièces visitées
+        List<Entry> opened = new LinkedList<>();
         List<Entry> closed = new ArrayList<>();
         INetwork<IRoom, Direction> network = RoomNetwork.getInstance();
 
         opened.add(pickRandomRoom());
 
         while (opened.size() != 0) {
-            final Entry src = opened.iterator().next();
+            final Entry src = pickRandom(opened);
 
-            Map<Direction, Entry> candidates = accessibleRoomsFrom(src, new ArrayList<>(opened), closed);
+            Map<Direction, Entry> candidates = accessibleRoomsFrom(src, opened, closed);
             if (candidates.size() > 0) {
                 // Choisir une direction
                 Direction d = pickRandom(new ArrayList<>(candidates.keySet()));
@@ -105,6 +152,11 @@ public class GrowingTreeGenerator implements IMazeGenerator {
     }
 
     // OUTILS
+
+    /**
+     * Sélectionne une pièce aléatoirement dans le labyrinthe et renvoie une nouvelle entrée à partir de celle-ci.
+     * @return Une nouvelle entrée, contenant la pièce sélectionnée et ses coordonnées dans le labyrinthe
+     */
     private Entry pickRandomRoom() {
         Random generator = new Random();
         int x = generator.nextInt(rooms.length);
@@ -112,7 +164,16 @@ public class GrowingTreeGenerator implements IMazeGenerator {
         return new Entry(x, y, rooms[x][y]);
     }
 
+    /**
+     * Sélection un objet de type T dans une liste de candidats.
+     * @param candidates la liste des candidats
+     * @param <T> le type des objets
+     * @return une candidat parmis ceux passés en paramètre
+     * @pre candidates != null && candidates.size() > 0
+     * @post candidates.contains(pickRandom(candidates))
+     */
     private <T> T pickRandom(List<T> candidates) {
+        assert candidates!= null && candidates.size() > 0;
         Random generator = new Random();
         return candidates.get(generator.nextInt(candidates.size()));
     }
@@ -125,6 +186,13 @@ public class GrowingTreeGenerator implements IMazeGenerator {
         return this.rooms[0].length;
     }
 
+    /**
+     * Indique si les coordonnées fournies sont bien des coordonnées valides pour le labyrinthe courant.
+     * @param x abscisse
+     * @param y ordonnée
+     * @return true si les coordonnées sont valides, false sinon
+     * @post isValidRoomCoordinates(x, y) <==> 0 <= x < getMazeWidth() && 0 <= y < getMazeHeight()
+     */
     private boolean isValidRoomCoordinates(int x, int y) {
         return 0 <= x && x < getMazeWidth() && 0 <= y && y < getMazeHeight();
     }
